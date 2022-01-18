8367da9f01a36e9a3df24dbf4557f171f12145c9
Destrox <p.kuniczak@gmail.com>
2022-01-18 01:22:25 +0100

    create template

---
+++ express_app/src/Server.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import logger from "./shared/Logger";
+import bodyParser from "body-parser";
+import BaseRouter from "./routers";
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+
+
+(async () => {
+    try {
+        const conn = await createConnection();
+        logger.info('Connection to database: ' + conn.isConnected)
+    } catch (e: any) {
+        logger.err(e.message)
+    }
+})()
+
+const app = express()
+
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use('/api', BaseRouter);
+
+app.listen(process.env.PORT, () => {
+    logger.info('Express server started on port: ' + process.env.PORT);
+});
+
+export default app;
